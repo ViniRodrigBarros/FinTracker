7eac4d8ed702c3e43704248cb80af5da11638364
ViniciusRBarros <vinicius.rodrigues@mobilibus.com.br>
2023-11-24 17:15:16 -0300

    login with json

--- fintracker/src/main/java/com/example/fintracker/controller/UserController.java
+++ fintracker/src/main/java/com/example/fintracker/controller/UserController.java
@@ -33,7 +33,6 @@ public class UserController {
 
         if (authService.authenticate(email, password)) {
             return ResponseEntity.ok("Login successful");
-
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
         }
